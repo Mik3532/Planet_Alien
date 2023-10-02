53901ac735339a77a837065101f772840139f96d
Mik3532 <142448439+Mik3532@users.noreply.github.com>
2023-10-02 08:24:33 -0300

    Game planeta alien.sql

--- Game planeta alien.sql
+++ Game planeta alien.sql
@@ -79,4 +79,50 @@ insert into tabelab (tipo) values ('criar');
 	
  
  select *from tabelab;
+
+CREATE TABLE historiajogo (
+    id int primary key auto_increment,
+    titulo varchar (120),
+    descricao longtext,
+    final varchar (120)
+);
+ 
+ 
+
+ 
+ 
+ insert into historiajogo (titulo,descricao,final) values ('Planeta Alien','No ano de 3250 a terra foi destruída por alienígenas do Planeta Alien. Esses extraterrestes
+eram desenvolvedores de tecnologia avançada em seu planeta, mas aprenderam com os
+humanos a linguagem de marcação HTML, a linguagem de estilização CSS e a linguagem de
+programação Java Script.
+Antes de destruir a terra por inteiro, escolheu um casal de cada Mamífero e fez uma mutação
+genética com a tecnologia do planeta Alien e as tecnologias aprendidas no planeta Terra:
+HTML, CSS e Java Script.
+Todos os Mamíferos mutantes, aceitaram tranquilamente a mutação genética, mas um e
+apenas um, chamado de Furão Mutante Guerreiro, não aceitou. Ele foi levado para o Planeta
+Alien, mas a sua memória ainda continuou na terra. Um dia essas memórias se misturaram
+com sua mutação genética e ele se revoltou contra os aliens.
+A revolta fez nascer missões. E cada missão ganha um nível de força que o torna mais poderoso
+aumentando a possibilidade de destruir mais aliens e assim salvar também os seus amigos
+terrestres.
+O jogo possuirá 10 missões:
+1. Encontrar Tags (força nível 1)
+a. O furão deve encontrar as principais “tags html” para matar um inimigo.
+2. Encontrar “Selectores” (força nível 2)
+3. Encontrar “Scripts” (força nível 3)
+4. Encontrar “Exceptions” (força nível 4)
+5. Encontrar “Conditions” (força nível 5)
+6. Encontrar “Loops” (força nível 6)
+7. Encontrar “Arrays” (força nível 7)
+8. Encontrar “Functions” (força nível 8)
+9. Encontrar “Objects” (força nível 9)
+10. Encontrar “Databases” (força nível 10)','Ao final de todas as missões, todos os “Aliens” serão destruídos e o Furão irá resgatar seus
+amigos e construir um novo planeta com justiça e paz.');
+ 
+
+ 
+
+
+
+ select*from historiajogo;
  
